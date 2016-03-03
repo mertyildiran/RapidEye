9c2513576efbbda2c6db119e4e1a164a3e4b17a1
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-03-03 21:16:46 +0200

    Chi-Square method added.

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 An Image Search Toolkit based on OpenCV.
 
 ### Version
-0.0.9
+0.1.0
--- rapideye/__init__.py
+++ rapideye/__init__.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import cv2
 import time
+import numpy
 
 def initiate():
 	ap = argparse.ArgumentParser()
@@ -11,6 +12,8 @@ def initiate():
 
 	reference = cv2.imread(args["query"],cv2.IMREAD_COLOR)
 
+	blank = numpy.empty_like(reference)
+
 	cv2.imshow("Query Image", reference)
 	cv2.moveWindow("Query Image",50,100)
 
@@ -19,32 +22,48 @@ def initiate():
 	reference_hist_saturation = cv2.calcHist([reference],[1],None,[256],[0,256])
 	reference_hist_value = cv2.calcHist([reference],[2],None,[256],[0,256])
 
+	blank_hist = cv2.calcHist([reference],[0],None,[256],[0,256])
+
 	for file in os.listdir(args["dataset"]):
 		if file.endswith(".png"):
 			full_path = args["dataset"] + file
 			img = cv2.imread(full_path,cv2.IMREAD_COLOR)
+
+			cv2.imshow("Similar Image", img)
+			cv2.moveWindow("Similar Image",600,100)
+			key = cv2.waitKey(1) & 0xFF
+
 			img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 			hist_hue = cv2.calcHist([img],[0],None,[256],[0,256])
 			hist_saturation = cv2.calcHist([img],[1],None,[256],[0,256])
 			hist_value = cv2.calcHist([img],[2],None,[256],[0,256])
 
+			print full_path
+
+			# Correlation
+
 			diff_hue = cv2.compareHist(reference_hist_hue,hist_hue,cv2.cv.CV_COMP_CORREL)
 			diff_saturation = cv2.compareHist(reference_hist_saturation,hist_saturation,cv2.cv.CV_COMP_CORREL)
 			diff_value = cv2.compareHist(reference_hist_value,hist_value,cv2.cv.CV_COMP_CORREL)
 
-			diff = (abs(diff_hue) + abs(diff_saturation) + abs(diff_value)) / 3
+			correlation_diff = (abs(diff_hue) + abs(diff_saturation) + abs(diff_value)) / 3
 
-			print full_path
-			print "Hue Diff: " + str(diff_hue)
-			print "Saturation Diff: " + str(diff_saturation)
-			print "Value Diff" + str(diff_value)
-			print "Average Diff: " + str(diff)
-			print "----------------------------"
-
-			img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
-			cv2.imshow("Similar Image", img)
-			cv2.moveWindow("Similar Image",600,100)
-			key = cv2.waitKey(1) & 0xFF
-
-			if diff > 0.7:
+			if correlation_diff > 0.7:
+				print "Found with CORRELATION"
 				time.sleep(3)
+				continue
+
+			# Chi-Square
+
+			diff_hue = cv2.compareHist(reference_hist_hue,hist_hue,cv2.cv.CV_COMP_CHISQR)
+			diff_saturation = cv2.compareHist(reference_hist_saturation,hist_saturation,cv2.cv.CV_COMP_CHISQR)
+			diff_value = cv2.compareHist(reference_hist_value,hist_value,cv2.cv.CV_COMP_CHISQR)
+
+			chi_diff = (abs(diff_hue) + abs(diff_saturation) + abs(diff_value)) / 3
+
+			print chi_diff
+
+			if chi_diff < 1000000.0 and correlation_diff > 0.6:
+				print "Found with combination of CORRELATION & CHI-SQUARE"
+				time.sleep(3)
+				continue
